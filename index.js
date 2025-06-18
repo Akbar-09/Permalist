caa1d58b8f781381301eee9be3b3983d9304e3fe
Akbar-09 <akbarkhan91367@gmail.com>
2025-06-18 18:22:02 +0530

    updated headers

--- index.js
+++ index.js
@@ -18,54 +18,87 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentHeaderId = 1;     // identify it as 1 to always open the first list (Today)
+
 let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
+  { id: 1, title: "Buy milk", header_id: 1},
+  { id: 2, title: "Finish homework", header_id:2},
+];
+let headers = [
+  {id:1, list_name: "Today"},
+  {id:2, list_name: "Week"},
+  {id:3, list_name: "Month"}
 ];
 
-app.get("/", async(req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
-  const items = result.rows;
-  console.log(result.rows);
+async function getItems(){
+  const result = await db.query(
+    "SELECT * FROM items WHERE header_id = $1 ORDER BY id ASC;",[currentHeaderId]
+  );
+  items = result.rows;
+  return items;
+}
 
-  res.render("index.ejs", {
-    listTitle: "Today",
+async function getCurrentHeader(){
+  const result = await db.query("SELECT * FROM headers");
+  headers = result.rows;
+  return headers.find((header)=> header.id == currentHeaderId);
+}
+
+//Read
+app.get("/", async(req, res) => {
+    const items = await getItems();
+    const headers1 = await getCurrentHeader(); 
+
+    res.render("index.ejs", {
+    listTitle: headers1.list_name,
     listItems: items,
-  });
-  } catch (err) {
-    console.log(err);
-  }
-  
+    headers1: headers
+    });
+ 
 });
 
+//Create
 app.post("/add", async(req, res) => {
   const item = req.body.newItem;
-  try {  
-  await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
-  res.redirect("/");
-  } catch (err) {
-    console.log(err);
-  }
-});
+  const headers1 = await getCurrentHeader();
 
-app.post("/edit", async(req, res) => {
-  const id = req.body.updatedItemId;
-  const title = req.body.updatedItemTitle;
   try {
-    await db.query("UPDATE items SET title = $1 WHERE id = $2", [title, id]);
-  res.redirect("/");
-  } catch (err) {
+    await db.query("INSERT INTO items (title, header_id) VALUES ($1,$2)",
+    [item, currentHeaderId]
+    );
+    res.redirect("/");
+  }catch(err){
     console.log(err);
   }
 });
 
+//Update
+app.post("/edit", async(req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try{
+    await db.query("UPDATE items SET title = ($1) WHERE id = $2", 
+    [item, id]
+    );
+    res.redirect("/");
+  }catch(err){
+    console.log(err);
+  }
+});
+
+//Heading
+app.post("/header", async (req, res) => {
+  currentHeaderId = req.body.header; //header id it could be 1,2,3 and will update the currentHeader var.
+  res.redirect("/");
+});
+
+//Delete
 app.post("/delete", async(req, res) => {
   const id = req.body.deleteItemId;
   try{
-    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    await db.query("DELETE FROM items WHERE id = $1",[id]);
     res.redirect("/");
-  } catch(err) {
+  }catch(err){
     console.log(err);
   }
 });
